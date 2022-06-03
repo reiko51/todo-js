6a50c5c4b988e478fa5486ea0d73fdb3299fc450
reiko51 <71922155+reiko51@users.noreply.github.com>
2022-06-03 11:30:20 +0900

    タスクの追加機能（ボタン）

--- src/index.js
+++ src/index.js
@@ -14,8 +14,24 @@ const onClickAdd = () => {
   //やっていることはこれ↓と同じ
   // console.log((document.createElement("li").innerText = "テスト中"));
 
+  //button（完了）タグ作成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  // completeButton.addEventListener("click", () => {
+  // alert("completeButton");
+  // });
+
+  //button（削除）タグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("deleteButton");
+  });
+
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   // console.log(div);
 
   //未完了リストに追加
@@ -25,6 +41,7 @@ const onClickAdd = () => {
   // console.log(ul);
 };
 
+//追加ボタンに関数を割り当てる
 document
   .getElementById("add-button")
   .addEventListener("click", () => onClickAdd());
